e597a89609c9ef180690c1be5749e2c1494b7666
Gregor Gramlich <gregor.gramlich@gmail.com>
2016-06-03 16:06:45 +0000

    create apache virtualhost sites configurations and enable them

--- docker/update-templates-and-run.sh
+++ docker/update-templates-and-run.sh
@@ -2,4 +2,22 @@
 
 dockerize -delims "<%:%>" -template /setup.local.php.tmpl:/app/setup.local.php
 
+function create_site_conf {
+  export SITE_NAME=$1
+  export SITE_DOMAIN=$2
+  if [ -n "${SITE_NAME}" ]; then
+    dockerize -delims "<%:%>" -template /respond-virtualhost.conf.tpl:/etc/apache2/sites-available/${SITE_DOMAIN}.conf
+    a2ensite ${SITE_DOMAIN}
+  fi
+}
+
+#http://stackoverflow.com/questions/10586153/split-string-into-an-array-in-bash
+IFS=',' read -r -a sites <<< "$VIRTUAL_HOST_RESPOND_SITES"
+IFS=',' read -r -a domains <<< "$VIRTUAL_HOST"
+
+for index in "${!sites[@]}"
+do
+  create_site_conf "${sites[index]}" "${domains[index]}"
+done
+
 /run.sh
